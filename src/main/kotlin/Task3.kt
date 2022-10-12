e69cfb389e6b06304d636129bfe62ccae5b7fb58
Anastasiya Matveyenka <a.matveyenka@gmail.com>
2022-10-12 23:37:00 +0200

    Task3 brickPasses doesn't work. Delete Task3

--- src/main/kotlin/Task3.kt
+++ src/main/kotlin/Task3.kt
@@ -1,4 +1,6 @@
 fun main (args: Array<String>) {
+    var min1: Int
+    var min2: Int
     println("Введите размеры кирпича")
     println("Введите длину кирпича")
     var input_a = readLine()
@@ -17,8 +19,42 @@ fun main (args: Array<String>) {
     var input_s = readLine()
     var s: Int = input_s!!.toInt()
     println("Кирпич размером $a  x $b x $c пройдёт в отверстие $r x $s")
-    fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int) {
-println("tjojhiyhj")
+    findminBrickSize()
+    compareSizes ()
+
+
+/*
+fun findmaxHoleSize(Int r, Int s)
+{
+    if (r>s){
+        var maxHoleSize=r
+    }
+    else {
+        var maxHoleSize=s
     }
 }
+*/
+fun findminBrickSize(Int a, Int b, Int c): Int {
 
+var max1: Int
+    if (a<b)  {
+     min1=a
+     max1=b
+}
+    else {
+        min1=b
+        max1=a
+    }
+    if (max1<c){
+        min2=max1
+    }
+    else {
+        min2=c
+    }
+    return min1
+    return min2
+}
+compareSizes (min1, min2, r, s){
+    if ((min1<r)
+}
+}
